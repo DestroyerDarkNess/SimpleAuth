94389c7d99b8840f19084c270e075098736e5e99
Pentesting-28 <mpenav28@gmail.com>
2023-02-02 17:24:57 -0400

    Making the user registration method functional implemented the corresponding logic

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -5,17 +5,51 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+/* IMPORTACION DE MODELOS*/
+use App\Models\User;
+
+/* OTRAS IMPORTACIONES */
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\Auth\{
+    RegisterRequest,
+    LoginRequest
+};
+use Exception;
+
 class AuthController extends Controller
 {
     /**
      * Method to register a users
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Http\Requests\Auth\RegisterRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function register(Request $request)
+    public function register(RegisterRequest $request)
     {
-        return "register";
+        try {
+            $user = User::create([
+                'name'      => $request->name,
+                'email'     => $request->email,
+                'password'  => Hash::make($request->password),
+                'coins'     => 0
+            ]);
+
+            $data = User::where('id', $user->id)->first();
+            $success = [
+                "token" => $data->createToken('systemWhatsapp')->plainTextToken,
+                "user"  => $data
+            ];
+
+            return response()->json([
+                'message' => 'Usuario creado con exito',
+                'data' => $success
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => 'auth.register.failed',
+                'message' => $e->getMessage(),
+            ], 505);
+        }
     }
 
     /**
